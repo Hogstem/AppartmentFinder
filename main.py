1f55bdb787106c5961ee7525eb69adfdc63ce0cd
jsl12 <32917998+jsl12@users.noreply.github.com>
2022-02-14 21:01:02 -0600

    created save_results and broke script execution into functions, disabled notifications

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
+# ! python3
+
 # This program can be turned into a .bat file and run once or multiple times a day with task scheduler to send you a desktop notification when a new apartment is available
 
-from datetime import date
+from pathlib import Path
+from typing import List, Dict, Iterable
 
-# ! python3
 import requests
 from bs4 import BeautifulSoup
-from notifypy import Notify
-from openpyxl import Workbook as book
-
-filename = "Apartment.xlsx"  # This creates the excel spreadsheet each time
-workbook = book()
-sheet = workbook.active
-c = 0  # this is a counter for looping through the URL's in the list
-li = []  # records links to rentals so that no repeats come up
-URL = ['Enter the craigslist URL for the area you are in, make sure the max apartment price is at at least 700$']
-Lis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U']
-today = date.today()
-o = 1
+from openpyxl import Workbook, load_workbook
 
 
-def result_gen(url: str):
-    soup = BeautifulSoup(
-        requests.get(url).content,
-        'lxml'
-    )
+def result_gen(url: str = 'Apartment.xlsx') -> Iterable[Dict[str, str]]:
+    soup = BeautifulSoup(requests.get(url).content, 'lxml')
+
     for res in soup.select('#search-results li.result-row div.result-info'):
         posted = res.select_one('time.result-date')
         link = res.select_one('h3 a')
@@ -39,36 +28,58 @@ def result_gen(url: str):
         }
 
 
-for url in URL_LIST:  # This cycles through the URL's if you have more than one
-    for i, res in enumerate(result_gen(url)):
-        if res['link'] not in li:
-            if res['location'] is not None:
-                if 10 <= res['price'] <= 1500:
+def save_results(path: Path, results: List[Dict]):
+    path = Path(path) if not isinstance(path, Path) else path
 
-                    li.append(res['link'])
+    if path.exists():
+        book = load_workbook(filename=str(path))
+    else:
+        book = Workbook()
+        sheet = book.worksheets[0]
 
-                    sheet['A1'] = 'Item Name'
-                    sheet['B1'] = 'Price'
-                    sheet['C1'] = 'Link'
-                    sheet['D1'] = 'Location'
-                    sheet['E1'] = 'Posted Date'
+        sheet.column_dimensions['A'].width = 63
+        sheet.column_dimensions['C'].width = 89
+        sheet.column_dimensions['D'].width = 29
 
-                    sheet.column_dimensions['A'].width = 63
-                    sheet.column_dimensions['C'].width = 89
-                    sheet.column_dimensions['D'].width = 29
+        headers = [
+            'Item Name',
+            'Price',
+            'Link',
+            'Location',
+            'Posted Date'
+        ]
 
-                    sheet[f'A{i}'] = res['name']
-                    sheet[f'B{i}'] = res['price']
-                    sheet[f'C{i}'] = res['link']
-                    sheet[f'D{i}'] = res['location']
-                    sheet[f'E{i}'] = res['date']
+        for i, h in enumerate(headers):
+            sheet.cell(row=1, column=i + 1).value = h
 
-                    workbook.save(filename=filename)
-                    # if the date of the posting is todays date it will notify your desktop
+    sheet = book.worksheets[0]
+    for i, res in enumerate(results):
+        first_free_row = sheet.max_row + 1
+        sheet.cell(row=first_free_row, column=1).value = res['name']
+        sheet.cell(row=first_free_row, column=2).value = res['price']
+        sheet.cell(row=first_free_row, column=3).value = res['link']
+        sheet.cell(row=first_free_row, column=4).value = res['location']
+        sheet.cell(row=first_free_row, column=5).value = res['date']
 
-                    if str(res['date']) == today.strftime('%b %d'):
-                        # https://github.com/ms7m/notify-py#usage
-                        notification = Notify()
-                        notification.title = res['name']
-                        notification.message = f"{res['link']}\n{res['price']}"
-                        notification.send()
+    book.save(filename=str(path))
+
+# if the date of the posting is todays date it will notify your desktop
+# if str(res['date']) == today.strftime('%b %d'):
+#     # https://github.com/ms7m/notify-py#usage
+#     notification = Notify()
+#     notification.title = res['name']
+#     notification.message = f"{res['link']}\n{res['price']}"
+#     notification.send()
+
+if __name__ == '__main__':
+    URL_LIST = ['Enter the craigslist URL for the area you are in, make sure the max apartment price is at at least 700$']
+
+    results = [
+        result
+        for url in URL_LIST
+        for result in result_gen(url)
+    ]
+
+    print(f'Found {len(results)} listings')
+
+    save_results('Apartment.xlsx', results)
